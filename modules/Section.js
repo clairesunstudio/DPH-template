d0f78c3e71b29c16ecbe25f61ce893974cc14907
Minghua Sun <clairesunstudio@gmail.com>
2017-03-07 14:33:11 -0500

    check data before render in Section

--- modules/Section.js
+++ modules/Section.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
-
+// const arr = [
+//   "Suspendisse non tempus justo. Pellentesque malesuada lectus augue, eu blandit ex hendrerit eget. In non convallis eros. Maecenas consectetur neque a nibh faucibus blandit. In quis urna sollicitudin, dictum sapien sit amet, rutrum metus. Donec sed risus neque. Ut a orci ex. Morbi laoreet suscipit tortor at ullamcorper.eu blandit ex hendrerit eget. In non convallis eros. Maecenas consectetur neque a nibh faucibus blandit. Pellentesque malesuada lectus augue, eu blandit ex hendrerit eget. In non convallis eros. Maecenas consectetur neque a nibh faucibus blandit.",
+//   "Tri-slider/scale showing the percentage of deaths broken down among the groups in each category (gender, age, and race). Also, it’s possible to add in the comparison to all deaths chart they have for each element in the QR."
+// ]
 var Section = React.createClass({
   propTypes: {  //propTypes and React.PropTypes have diff capitalizations
     id:   React.PropTypes.string.isRequired,
-    paragraph:     React.PropTypes.array.isRequired
+    paragraphs:     React.PropTypes.array.isRequired
   },
   render: function(){
-    var obj = this.props.paragraph;
-    console.log(typeof(obj));
+    const obj = this.props.paragraphs[this.props.index];
+    // console.log(typeof(obj)+obj);
+     console.log(obj);
+
     //var paragraphs = this.props.paragraph[this.props.index];
     //console.log(paragraphs);
     //var arr = Object.keys(obj).map(key => obj[key])
-    // Object.keys(obj).map(key => obj[key])
-    var paragraphList = this.props.paragraph.map((para,i) => <p key={"para_" + i}>{para.paragraph}</p>);
-
-
+    var paragraphList;
+    if (obj){
+      paragraphList = obj.paragraphs.map((para,i) => <p key={"para_" + i}>{para}</p>);
+    }
       return(
         <div>
           <h3 className="ma__comp-heading  " id={this.props.id.toLowerCase()}>
