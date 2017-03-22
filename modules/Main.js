76a4d8f2b2f5145a2efb710c02f3cec375bf9fdc
Minghua Sun <clairesunstudio@gmail.com>
2017-03-22 09:55:57 -0400

    customize recharts package

--- modules/Main.js
+++ modules/Main.js
@@ -4,14 +4,24 @@ import Section from './Section'
 import SplitColumns from './SplitColumns'
 import Figure from './Figure'
 import Paragraph from './Paragraph'
-import {ResponsiveContainer,BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+import {ResponsiveContainer,BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'rechart-custom-package'
 
 var Main = React.createClass({
+
   propTypes: {  //propTypes and React.PropTypes have diff capitalizations
     bar:     React.PropTypes.array.isRequired,
     line:     React.PropTypes.array.isRequired
   },
   render: function(){
+    const getPercent = (value, total) => {
+        const ratio = total > 0 ? value / total : 0;
+
+        return toPercent(ratio, 2);
+    };
+
+    const toPercent = (decimal, fixed = 0) => {
+        return `${(decimal * 100).toFixed(fixed)}%`;
+    };
       return(
         <main id="main-content" tabIndex={-1}>
           <div className="main-content main-content--two">
@@ -28,22 +38,22 @@ var Main = React.createClass({
                 <div id="death-all-month-chart"></div>
                 <ResponsiveContainer minHeight={600}>
                   <BarChart data={this.props.bar} margin={{top: 20, right: 20, left: 10, bottom: 5}}>
-                       <XAxis dataKey="Month" style={{fontSize: "0.71em"}}/>
-                       <YAxis tickCount = {10} />
+                       <XAxis dataKey="Month" fontSize = "0.71em"/>
+                       <YAxis tickCount = {10} fontSize = "0.71em"/>
                        <CartesianGrid strokeDasharray="3 3"/>
                        <Tooltip/>
                        <Legend />
-                       <Bar dataKey="Estimated" stackId="a" fill="#535353" />
-                       <Bar dataKey="Confirmed" stackId="a" fill="#43956f" />
+                       <Bar dataKey="Estimated" stackId="a" fill="#535353"/>
+                       <Bar dataKey="Confirmed" stackId="a" fill="#43956f"/>
                   </BarChart>
                 </ResponsiveContainer>
                 <ResponsiveContainer minHeight={600} height={400}>
                   <LineChart data={this.props.line}>
-                    <XAxis dataKey="date" style={{fontSize: "0.71em"}}/>
-                    <YAxis tickCount = {5} />
+                    <XAxis dataKey="date" fontSize = "0.71em" />
+                    <YAxis tickCount = {5} tickFormatter={toPercent} fontSize = "0.71em" />
                     <CartesianGrid strokeDasharray="3 3"/>
                     <Tooltip/>
-                    <Legend />
+                    <Legend/>
                     <Line dataKey="Both Fentanyl & Heroin" stroke="#5a707a" />
                     <Line dataKey="Only Fentanyl" stroke="#b71c1c" activeDot={{r: 8}}/>
                     <Line dataKey="Only Heroin" stroke="#0071bc"/>
