1ba35cc5a31fe37e28f98e87ae22c4291df041f2
Minghua Sun <clairesunstudio@gmail.com>
2017-03-24 14:02:20 -0400

    add and style homepage

--- modules/App.js
+++ modules/App.js
@@ -5,10 +5,6 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <ul role="nav">
-          <li><NavLink to="/repos" onlyActiveOnIndex>Home</NavLink></li>
-          <li><NavLink to="/repos/2017/3">Repos</NavLink></li>
-        </ul>
         {this.props.children}
       </div>
     )
--- modules/Home.js
+++ modules/Home.js
@@ -1,7 +1,53 @@
 import React from 'react'
+import NavLink from './NavLink'
 
 export default React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const year = event.target.elements[0].value
+    const quarter = event.target.elements[1].value
+    const path = `/repos/${year}/${quarter}`
+    this.context.router.push(path)
+  },
+
   render() {
-    return <div>Home</div>
+    return (
+      <div>
+
+      <main id="main-content" className="ma__narrow-template ma__narrow-template--left ma__narrow-template--green" tabIndex={-1}>
+              <header className="ma__narrow-template__header">
+                <div className="ma__site-logo">
+                  <a href="/" title="Mass Gov home page">
+                    <img src="/assets/images/stateseal.png" alt="Mass Gov" width={75} height={75} />
+                  </a>
+                </div>
+              </header>
+              <div className="ma__narrow-template__container">
+                <div className="ma__narrow-template__content">
+                  <section className="ma__error-page">
+                    <div className="ma__error-page__label" />
+                    <h1 className="ma__error-page__title">DPH Quarterly Report</h1>
+                    <h2 className="ma__error-page__message">The Massachusetts Department of Public Health collects overdose death data to track the progression of the opioid crisis and target services to especially hard-hit communities. The data in this report is updated quarterly.</h2>
+                    <ul role="nav">
+                      <li className="ma__main-nav__item js-main-nav-toggle"><NavLink className="ma__main-nav__top-link" to="/" onlyActiveOnIndex>Home</NavLink></li>
+                      <li className="ma__main-nav__item js-main-nav-toggle"><NavLink className="ma__main-nav__top-link" to="/repos/2017/3">Newest Report</NavLink></li>
+                      <li className="ma__main-nav__item js-main-nav-toggle"><NavLink className="ma__main-nav__top-link" to="/repos/2017/2">Last Quarter</NavLink></li>
+                    </ul>
+                    <form onSubmit={this.handleSubmit}>
+                      <input type="number" min="2016" max="2017" placeholder="year"/> / {' '}
+                      <input type="number" min="1" max="4" placeholder="quarter"/>{' '}
+                      <button className="ma__button-alt ma__button-alt--small" type="submit">View</button>
+                    </form>
+                  </section>
+
+                  </div>
+              </div>
+            </main>
+      </div>
+    )
   }
 })
