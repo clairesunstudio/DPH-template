dfc0399459d419666438c0675679aca0d48a220c
Minghua Sun <clairesunstudio@gmail.com>
2017-05-04 11:40:44 -0400

    add back App in routes

--- modules/routes.js
+++ modules/routes.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory, IndexRedirect } from 'react-router'
+import App from './App'
 import Content from './Content'
 import NotFound from './NotFound'
 
 
 module.exports = (
   <Router history={browserHistory}>
+    <Route path="/" component={App}>
       <IndexRedirect to="/2017/3" />
       <Route name="report" path="/:year/:quarter" component={Content}/>
       <Route path="*" component={NotFound} />
+    </Route>
   </Router>
 )
