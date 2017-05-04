940c941ffb33e738783dfb46d0ff3f3a120f5728
Minghua Sun <clairesunstudio@gmail.com>
2017-05-04 11:41:19 -0400

    use hashHistory instead of browseHistory

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, browserHistory } from 'react-router'
+import { Router, hashHistory } from 'react-router'
 // import routes and pass them into <Router/>
 import routes from './modules/routes'
 
 render(
-  <Router routes={routes} history={browserHistory}/>,
+  <Router routes={routes} history={hashHistory}/>,
   document.getElementById('app')
 )
