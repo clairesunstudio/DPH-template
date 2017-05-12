0969743360a0c2b92a30c6cc557526753ec070cb
Minghua Sun <clairesunstudio@gmail.com>
2017-05-12 13:56:32 -0400

    prevent push adding to the url

--- modules/Dropdown.js
+++ modules/Dropdown.js
@@ -15,7 +15,7 @@ var Dropdown = React.createClass({
           console.log("local");
     } else {
       subdir = arr[3] || ""
-      myRoot = subdir + "/";
+      myRoot = "/" + subdir;
       console.log(myRoot)}
 
 
@@ -25,10 +25,12 @@ var Dropdown = React.createClass({
              <label htmlFor="color-select" className="ma__select-box__label">View other quarterly reports</label>
              <div className="ma__select-box__field">
                <select className="ma__select-box__select">
-                  <option value={`2017/3`}>Q3 2017</option>
-                  <option value={`2017/2`}>Q2 2017</option>
-                  <option value={`2017/1`}>Q1 2017</option>
+                 <option value="">none</option>
+                  <option value={`${myRoot}/2017/3`}>Q3 2017</option>
+                  <option value={`${myRoot}/2017/2`}>Q2 2017</option>
+                  <option value={`${myRoot}/2017/1`}>Q1 2017</option>
                   <option value="older">Older</option>
+
                </select>
                <div className="ma__select-box__link">
                  <span>{this.props.placeholder}</span>
