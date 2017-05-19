a243d13b03a124f8586575089a8dadada3bfb95c
Minghua Sun <clairesunstudio@gmail.com>
2017-05-19 16:16:37 -0400

    load report only if it exists in /content and it's loaded
    #1

--- modules/Content.js
+++ modules/Content.js
@@ -1,8 +1,9 @@
- import React from 'react'
+import React from 'react'
 import axios from 'axios'
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
+import Logo from './Logo'
 import NotFound from './NotFound'
 
 export default React.createClass({
@@ -16,13 +17,15 @@ export default React.createClass({
             data_bar_all: [],
             data_bar_unintent: [],
             data_line_drug: [],
-            data_line_ma: []
+            data_line_ma: [],
+            list: null
         }
     },
 
 
     componentWillMount: function() {
-            this.fetchContent()
+            this.fetchContent();
+            this.fetchList();
         },
 
         componentDidUpdate(prevProps) {
@@ -39,7 +42,7 @@ export default React.createClass({
                     axios.get('../../data/_' + this.props.params.year + '_' + this.props.params.quarter +'/bar-all-death.json'),
                     axios.get('../../data/_' + this.props.params.year + '_' + this.props.params.quarter +'/bar-unintent-death.json'),
                     axios.get('../../data/_' + this.props.params.year + '_' + this.props.params.quarter +'/line-drug-present.json'),
-                    axios.get('../../data/_' + this.props.params.year + '_' + this.props.params.quarter +'/line-ma-unintent.json'),
+                    axios.get('../../data/_' + this.props.params.year + '_' + this.props.params.quarter +'/line-ma-unintent.json')
                 ])
                 .then(axios.spread(function(result, bar_all, bar_unintent, line_drug, line_ma) {
                     _this.setState({
@@ -62,9 +65,46 @@ export default React.createClass({
             this.ignoreLastFetch = true
         },
 
+        fetchList() {
+          var _this = this;
+          axios.get('../../content/').then(function(files){
+          _this.setState({list: files.data})
+          })
+        },
+        printList(list) {
+          var exist = [];
+            for (var i=0; i<list.length; i++){
+            var file = {};
+            var filename = list[i].split(/[_.]/);
+            file.year = filename[1];
+            file.quarter = filename[2];
+            exist.push(file);
+          };
+          return exist
+        },
+
+        containsContent(obj, list) {
+          for (var i=0; i<list.length; i++) {
+            if ((list[i].year === obj.year) && (list[i].quarter === obj.quarter)){
+              return true;
+            }
+          }
+          return false;
+        },
+
         render() {
-            const {paragraphs} = this.state;
-            return (paragraphs) ? this.renderContent() : this.renderLoader();
+            const {list} = this.state;
+            var input = this.props.params;
+            var isExisted, isLoaded;
+            if(list){isLoaded=true}else{isLoaded=false};
+            if(isLoaded){
+                console.log("isLoaded");
+                var existed = this.printList(list);
+                isExisted = this.containsContent(input, existed);
+                if(isExisted) {
+                  console.log("isExisted")
+                  return this.renderContent()} else {return this.renderNotFound()}
+              } else {return this.renderLoader()}
         },
 
         renderContent() {
@@ -86,9 +126,15 @@ export default React.createClass({
             )
       },
 
+      renderNotFound() {
+        return (
+          <NotFound />
+        )
+      },
+
         renderLoader() {
           return (
-            <NotFound />
+            <Logo class={"ma__site-logo"} width={70} height={70}/>
           )
         }
 
